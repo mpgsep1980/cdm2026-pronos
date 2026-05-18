68ef45750e54d61ef8b0f7f6d447601d9a609a9b
Sébastien <sebdusnes@gmail.com>
2026-05-18 12:07:14 +0200

    resultats.js : suppression de tous les scores test

--- resultats.js
+++ resultats.js
@@ -2,9 +2,6 @@
 // NE PAS ÉDITER MANUELLEMENT
 
 // === PHASE DE GROUPES ===
-OFFICIEL_2026.scores["677633"] = { sA: 0, sB: 3, termine: true, live: false, statut: "Terminé" };
-OFFICIEL_2026.scores["677634"] = { sA: 2, sB: 0, termine: true, live: false };
-OFFICIEL_2026.scores["689422"] = { sA: 8, sB: 0, termine: true, live: false };
 
 // === TABLEAU 1/16e (alimenté manuellement après les groupes) ===
 // OFFICIEL_2026.qualifies["team_16_687045_A"] = "Mexique";
