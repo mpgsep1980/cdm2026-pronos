4ee46ab136cbc22c2bc32fc4026d84b0bc026e24
Sébastien <sebdusnes@gmail.com>
2026-07-06 09:16:41 +0200

    Live resultats 09:16

--- resultats.js
+++ resultats.js
@@ -1,4 +1,4 @@
-// resultats.js — Généré le 06/07/2026 04:40 par fetch_resultats.py
+// resultats.js — Généré le 06/07/2026 09:16 par fetch_resultats.py
 // NE PAS ÉDITER MANUELLEMENT
 
 // === PHASE DE GROUPES ===
